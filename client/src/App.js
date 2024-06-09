421b446eff892cafd60e79f92edb66d7e3da9906
YetronLives <125398855+YetronLives@users.noreply.github.com>
2024-06-08 20:26:53 -0400

    setup template

--- client/src/App.js
+++ client/src/App.js
@@ -13,24 +13,26 @@ function App() {
   }, []);
 
   return (
-    <div>{(typeof backendData.users === 'undefined') ? (<p>Loading...</p>): 
+    <div>
+      <nav id="desktop-nav">
+        <div class="logo">Colorstack KSU</div>
+        
+        <div>
+            <ul class="nav-links">
+                <li><a href="#about">About</a></li>
+                <li><a href="#experience">Events</a></li>
+                <li><a href="#projects">E-Board</a></li>
+                <li><a href="#contact">Sponsors</a></li>
+            </ul>
+        </div>
+    </nav>
+    <hr></hr>
+      {(typeof backendData.users === 'undefined') ? (<p>Loading...</p>): 
     (
       backendData.users.map((user, i)=> (
         <p key={i}>{user}</p>
       ))
     )}
-    <nav id="desktop-nav">
-        <div class="logo">Constant Nortey Jr.</div>
-        
-        <div>
-            <ul class="nav-links">
-                <li><a href="#about">About</a></li>
-                <li><a href="#experience">Experience</a></li>
-                <li><a href="#projects">Projects</a></li>
-                <li><a href="#contact">Contact</a></li>
-            </ul>
-        </div>
-    </nav>
     </div>
 
   )
